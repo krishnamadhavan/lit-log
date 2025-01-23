d9c9e9bda850f1c2c144644d56d4efb15f29c601
Krishna Madhavan <krishnamadhavan07@gmail.com>
2025-01-23 13:37:06 +0530

    feat: create a root endpoint for liveness check

--- server/main.go
+++ server/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
+
 
 func main() {
-	fmt.Println("Hello Krishna")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := r.Run()
+	if err != nil {
+		return
+	} // listen and serve on 0.0.0.0:8080
 }
